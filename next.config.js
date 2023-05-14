ca6e83767025919f667e4c31b8f53a9f82a51088
lukew3 <lukew25073@gmail.com>
2023-05-13 23:22:40 -0400

    Remove styles from tsx and fix next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { loader: 'custom' },
+  images: { loader: 'akamai', path: '' },
 }
 
 module.exports = nextConfig
