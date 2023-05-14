606d5717bd8676c36108c0b1e7b0916ee3ba72e3
lukew3 <lukew25073@gmail.com>
2023-05-14 03:37:54 +0000

    Deploying to gh-pages from @ lukew3/osu-room-availability@4f1cd1327cad7babcf283cabca5a20d4c51c0b43 🚀

---
+++ _next/static/p3qRXLPa4b4HfwQ66iq_T/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1650647fea74912c.css","static/chunks/pages/index-8ae0c623853a340d.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
