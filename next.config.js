4bea6e6909ad3b74736295edba67a4ec63b5a644
lukew3 <lukew25073@gmail.com>
2023-05-13 23:27:12 -0400

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { loader: 'akamai', path: '' },
+  images: { loader: 'custom' },
 }
 
 module.exports = nextConfig
