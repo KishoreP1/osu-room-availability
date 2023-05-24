3dfd81d23e6840ba52b61d84dbec8294bdd2e021
KishoreP1 <42980802+KishoreP1@users.noreply.github.com>
2023-05-24 17:28:13 +0000

    Deploying to gh-pages from @ KishoreP1/osu-room-availability@abc78ec3b517f51512d4d9e1cc662d5b0a299a7e 🚀

---
+++ _next/static/2iQItChXpI05d_eWQphPN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c84e21ae46eb05fa.css","static/chunks/pages/index-3b1ace7bf699aa31.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
